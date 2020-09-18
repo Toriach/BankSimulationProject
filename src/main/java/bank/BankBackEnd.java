a5bad7bbf948ac7ebc63ca87feb8bc5f89205d3f
Toriach <sowa125@gmail.com>
2020-09-18 23:00:49 +0200

    version 0.9

--- src/main/java/bank/BankBackEnd.java
+++ src/main/java/bank/BankBackEnd.java
@@ -16,11 +16,44 @@ public class BankBackEnd {
 
         createClientAccount("Toriach Corp", "", "", 1);
 
+        createBankAccount(0, BankDatabase.clientsList.get(1));
+        createBankAccount(0, BankDatabase.clientsList.get(1));
 
-        BankDatabase.accountsList.get(0).setBalance(0);
-       
-        removeBankAccountWithZeroBalance();
-        removeClientAccountWithoutBankAccount();
+        BankDatabase.accountsList.get(3).setBalance(127.45);
+        BankDatabase.accountsList.get(4).setBalance(24.4);
+        //   BankDatabase.accountsList.get(0).setBalance(0);
+
+        // removeBankAccountWithZeroBalance();
+        // removeClientAccountWithoutBankAccount();
+
+        System.out.println(BankDatabase.accountsList.get(2).getBalance());
+        receiveExternalTransaction(BankDatabase.accountsList.get(2).getAccountNumber(), 22.36);
+        System.out.println(BankDatabase.accountsList.get(2).getBalance());
+        System.out.println("--------------------------");
+
+        receiveExternalTransaction("blabla", 22.36);
+        System.out.println("--------------------------");
+
+        receiveInternalTransaction(BankDatabase.accountsList.get(2).getAccountNumber(), BankDatabase.accountsList.get(0).getAccountNumber(), 15.4);
+        System.out.println("--------------------------");
+        receiveInternalTransaction(BankDatabase.accountsList.get(2).getAccountNumber(), BankDatabase.accountsList.get(0).getAccountNumber(), 55);
+        System.out.println("--------------------------");
+
+        withdrawFounds(BankDatabase.accountsList.get(1).getAccountNumber(), 50);
+        System.out.println("--------------------------");
+        withdrawFounds(BankDatabase.accountsList.get(1).getAccountNumber(), 5);
+
+        System.out.println("--------------------------");
+        showListOfClientBankAccounts(BankDatabase.clientsList.get(1));
+
+        System.out.println("--------------------------");
+        showListOfClientBankAccountsWithBalance(BankDatabase.clientsList.get(1));
+
+        System.out.println("--------------------------");
+        showAllBankAccounts();
+
+        System.out.println("--------------------------");
+        showAllBankAccountsWithBalance();
     }
 
     private void createClientAccount(String name, String surname, String personalIdentityNumber, int AccountTypeById) {
@@ -46,6 +79,105 @@ public class BankBackEnd {
             BankDatabase.accountsList.add(account);
         } else {
             System.out.println("Account creating failed!");
+        }
+    }
+
+    public void receiveInternalTransaction(String sendingAccountNumber, String receivingAccountNumber, double amountToTransfer) {
+        /**
+         * tak, wiem, rozbić to na mniejsze metody i mniej ifów :)
+         */
+        boolean sendingAccountExist = false;
+        boolean receivingAccountExist = false;
+        int sendingAccountINDEX = -1;
+        int receivingAccountINDEX = -1;
+
+        for (int i = 0; i < BankDatabase.accountsList.size(); i++) {
+            if (BankDatabase.accountsList.get(i).getAccountNumber().equals(sendingAccountNumber)) {
+                sendingAccountExist = true;
+                sendingAccountINDEX = i;
+                break;
+            }
+        }
+        for (int i = 0; i < BankDatabase.accountsList.size(); i++) {
+            if (BankDatabase.accountsList.get(i).getAccountNumber().equals(receivingAccountNumber)) {
+                receivingAccountExist = true;
+                receivingAccountINDEX = i;
+                break;
+            }
+        }
+        if (!sendingAccountExist) {
+            System.out.println("ERROR , sending from non existing account?");
+        } else if (!receivingAccountExist) {
+            System.out.println("ERROR , target account don't exist!");
+        } else if (sendingAccountExist && receivingAccountExist) {
+            if (BankDatabase.accountsList.get(sendingAccountINDEX).getBalance() >= amountToTransfer) {
+                BankDatabase.accountsList.get(sendingAccountINDEX).setBalance(BankDatabase.accountsList.get(sendingAccountINDEX).getBalance() - amountToTransfer);
+                System.out.println("transferring " + amountToTransfer + " zł from account number: " + BankDatabase.accountsList.get(sendingAccountINDEX).getAccountNumber()
+                        + " to account number: " + BankDatabase.accountsList.get(receivingAccountINDEX).getAccountNumber());
+
+                BankDatabase.accountsList.get(receivingAccountINDEX).setBalance(BankDatabase.accountsList.get(receivingAccountINDEX).getBalance() + amountToTransfer);
+                System.out.println("Account number: " + BankDatabase.accountsList.get(receivingAccountINDEX).getAccountNumber()
+                        + " received " + amountToTransfer + " zł from accound number: " + BankDatabase.accountsList.get(sendingAccountINDEX).getAccountNumber());
+            } else {
+                System.out.print("You cannot sand " + amountToTransfer + " zł, because You only have ");
+                System.out.printf("%.2f zł in Your account!\n", BankDatabase.accountsList.get(sendingAccountINDEX).getBalance());
+            }
+        }
+    }
+
+    public void receiveExternalTransaction(String receivingAccountNumber, double amountToTransfer) {
+        int checkIfTargetAccountWasFound = 0;
+        for (Account targetBankAccount : BankDatabase.accountsList) {
+            if (targetBankAccount.getAccountNumber().equals(receivingAccountNumber)) {
+                targetBankAccount.setBalance(targetBankAccount.getBalance() + amountToTransfer);
+                System.out.println(amountToTransfer + " zł successfully transferred!");
+                checkIfTargetAccountWasFound = 1;
+            }
+        }
+
+        if (checkIfTargetAccountWasFound == 0) {
+            System.out.println("Target account not found!");
+        }
+    }
+
+    public void withdrawFounds(String accountNumberToWithdraw, int amount) {
+        int checkIfTargetAccountWasFound = 0;
+        for (Account accountToWithdraw : BankDatabase.accountsList) {
+            if (accountToWithdraw.getAccountNumber().equals(accountNumberToWithdraw)) {
+                if (accountToWithdraw.getBalance() >= amount) {
+                    accountToWithdraw.setBalance(accountToWithdraw.getBalance() - amount);
+                    System.out.println("You withdrawn " + amount + " zł from account number: " + accountToWithdraw.getAccountNumber() + " new balance: " + accountToWithdraw.getBalance());
+                } else {
+                    System.out.print("You cannot withdraw " + amount + " zł, because You only have ");
+                    System.out.printf("%.2f zł in Your account!\n", accountToWithdraw.getBalance());
+                }
+                checkIfTargetAccountWasFound = 1;
+            }
+        }
+        if (checkIfTargetAccountWasFound == 0) {
+            System.out.println("Target account not found!");
+        }
+    }
+
+    public void showListOfClientBankAccounts(ClientAccount clientAccount) {
+        System.out.println("Client: " + clientAccount.getName() + " " + clientAccount.getSurname() + " ID: " + clientAccount.getClientID());
+        System.out.println("Have " + clientAccount.getClientAccountsIDList().size() + " account/s.");
+        System.out.println("Account/s list: ");
+        for (int i = 0; i < clientAccount.getClientAccountsIDList().size(); i++) {
+            System.out.println("   - " + clientAccount.getClientAccountsIDList().get(i));
+        }
+    }
+
+    public void showListOfClientBankAccountsWithBalance(ClientAccount clientAccount) {
+        System.out.println("Client: " + clientAccount.getName() + " " + clientAccount.getSurname() + " ID: " + clientAccount.getClientID());
+        System.out.println("Have " + clientAccount.getClientAccountsIDList().size() + " account/s.");
+        System.out.println("Account/s list with balance: ");
+        for (int i = 0; i < clientAccount.getClientAccountsIDList().size(); i++) {
+            for (int j = 0; j < BankDatabase.accountsList.size(); j++) {
+                if (clientAccount.getClientAccountsIDList().get(i).equals(BankDatabase.accountsList.get(j).getAccountNumber())) {
+                    System.out.println("   - " + BankDatabase.accountsList.get(j).getAccountNumber() + "  - " + BankDatabase.accountsList.get(j).getBalance() + " zł.");
+                }
+            }
         }
     }
 
@@ -75,4 +207,15 @@ public class BankBackEnd {
         }
         BankDatabase.clientsList.removeAll(temporalClientAccountsList);
     }
+
+    public void showAllBankAccounts() {
+        System.out.println("List of all account in bank: ");
+        BankDatabase.accountsList.stream().forEach(account -> System.out.println("   - " + account.getAccountNumber()));
+    }
+
+    public void showAllBankAccountsWithBalance() {
+        System.out.println("List of all account in bank and balance: ");
+        BankDatabase.accountsList.stream().forEach(account -> System.out.printf("   - %s %.2f zł\n" , account.getAccountNumber(), account.getBalance()));
+    }
+
 }
